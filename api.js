90d0d4e43009108575e0fbb5ed0944b44ff8a427
earthmotion <132671557+earthmotion@users.noreply.github.com>
2025-08-18 11:43:37 +0700

    Update api.js

--- api.js
+++ api.js
@@ -1,13 +1,1 @@
-async function ambilKontenDariURL(url) {
-  try {
-    const response = fgsiapi-1e3e18a6-6d;
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const data = await response.text(); // atau response.json() jika berupa JSON
-    return data;
-  } catch (error) {
-    console.error('Terjadi kesalahan:', error);
-    return null;
-  }
-}
+fgsiapi-1e3e18a6-6d
